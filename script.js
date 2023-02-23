68cfad1da453ed9434bd8977bfe29589672b4f57
Diana-0302 <di.gorkavaya@gmail.com>
2023-02-23 22:56:13 +0200

    add changes

---
+++ script.js
@@ -0,0 +1,21 @@
+const a = Number (prompt('Введите первое число'));
+const b = Number (prompt('Введите второе число'));
+
+
+console.log('Calculations are finished!');
+
+const sum = a + b;
+
+console.log('Sum: ' + a + ' + ' + b + ' = ' + sum);
+
+const diff = a - b;
+
+console.log('Diff: ' + a + ' - ' + b + ' = ' + diff);
+  
+const mult = a * b;
+
+console.log('Mult: ' + a + ' * ' + b + ' = ' + mult);
+
+const div = a / b;
+
+console.log('Div: ' + a + ' / ' + b + ' = ' + div);
