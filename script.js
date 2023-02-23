45ef8a9681ac84b381c729e38ed763889aa5e5a3
Diana-0302 <di.gorkavaya@gmail.com>
2023-02-23 21:49:30 +0200

    add changes

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Hello!");
+
+const userName = prompt("What is your name?");
+const year = prompt("What is your birth year?");
+const country = prompt("Where are you from?");
+
+const age = 2023 - year;
+
+alert("Nice to meet you, " + userName + "! You are " + age + " years old! You are from " + country + "!");
