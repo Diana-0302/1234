85ed06809490beada3ca27e5f19a7d09c1a9a9d0
Diana-0302 <di.gorkavaya@gmail.com>
2023-03-01 22:30:31 +0200

    add changes

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-const a = Number (prompt('Введите первое число'));
-const b = Number (prompt('Введите второе число'));
 
 
-console.log('Calculations are finished!');
+const operation = prompt ('Выберите операцию (введите символ(ы)): \n Сложение: + \n Вычитание: - \n Умножение: * \n Деление: / \n Sin: sin \n Cos: cos \n Возведение в степень: pow');
 
-const sum = a + b;
+let a;
+let b;
 
-console.log('Sum: ' + a + ' + ' + b + ' = ' + sum);
 
-const diff = a - b;
-
-console.log('Diff: ' + a + ' - ' + b + ' = ' + diff);
-  
-const mult = a * b;
-
-console.log('Mult: ' + a + ' * ' + b + ' = ' + mult);
-
-const div = a / b;
-
-console.log('Div: ' + a + ' / ' + b + ' = ' + div);
+if (operation === '+'){
+ a = Number (prompt ('Введите первое число'));
+ b = Number (prompt ('Введите второе число'));
+ console.log (a + b);
+} else if (operation === '-') {
+  a = Number (prompt ('Введите первое число'));
+  b = Number (prompt ('Введите второе число'));
+  console.log (a - b);
+} else if (operation === '*') {
+  a = Number (prompt ('Введите первое число'));
+  b = Number (prompt ('Введите второе число'));
+  console.log (a * b);
+} else if (operation === '/') {
+  a = Number (prompt ('Введите первое число'));
+  b = Number (prompt ('Введите второе число'));
+  console.log (a / b);
+} else if (operation === 'sin') {
+  a = Number (prompt ('Введите число в радианах'));
+  console.log (Math.sin(a));
+} else if (operation === 'cos') {
+  a = Number (prompt ('Введите число в радианах'));
+  console.log (Math.cos(a));
+} else if (operation === 'pow') {
+  a = Number (prompt ('Введите число'));
+  b = Number (prompt ('Введите степень'));
+  console.log (Math.pow(a, b));
+} else {
+  alert ('Try again!');
+}
