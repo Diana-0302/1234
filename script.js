9a726fa0bc55d148ad1d94f583cc6ccf33781d29
Diana-0302 <di.gorkavaya@gmail.com>
2023-03-05 19:07:21 +0200

    add changes

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
-
-
 const operation = prompt('Выберите операцию (введите символ(ы)): \n Сложение: + \n Вычитание: - \n Умножение: * \n Деление: / \n Sin: sin \n Cos: cos \n Возведение в степень: pow');
 
 let a;
 let b;
 
+switch(operation) {
+  case '+':
+  case '-':
+  case '*': 
+  case '/': 
+  case 'pow':
+    a = Number(prompt('Введите первое число'));
+    b = Number(prompt('Введите второе число'));
+    break;
+  case 'sin':
+  case 'cos':
+    a = Number(prompt('Введите число в радианах'));
+    break;
+  default: alert('Try again!');
+}
 
-if (operation === '+'){
- a = Number(prompt('Введите первое число'));
- b = Number(prompt('Введите второе число'));
- console.log(a + b);
-} else if (operation === '-') {
-  a = Number(prompt('Введите первое число'));
-  b = Number(prompt('Введите второе число'));
-  console.log(a - b);
-} else if (operation === '*') {
-  a = Number(prompt('Введите первое число'));
-  b = Number(prompt('Введите второе число'));
-  console.log(a * b);
-} else if (operation === '/') {
-  a = Number(prompt('Введите первое число'));
-  b = Number(prompt('Введите второе число'));
-  console.log(a / b);
-} else if (operation === 'sin') {
-  a = Number(prompt('Введите число в радианах'));
-  console.log(Math.sin(a));
-} else if (operation === 'cos') {
-  a = Number(prompt('Введите число в радианах'));
-  console.log(Math.cos(a));
-} else if (operation === 'pow') {
-  a = Number(prompt('Введите число'));
-  b = Number(prompt('Введите степень'));
-  console.log(Math.pow(a, b));
-} else {
-  alert('Try again!');
+switch(operation) {
+  case '+': 
+    console.log(a + b);
+    break;
+  case '-': 
+    console.log(a - b);
+    break;
+  case '*': 
+    console.log(a * b);
+    break;
+  case '/': 
+    console.log(a / b);
+    break;
+  case 'pow':
+    console.log(Math.pow(a, b));
+    break;
+  case 'sin':
+    console.log(Math.sin(a));
+    break;
+  case 'cos':
+    console.log(Math.cos(a));
+    break;
 }
