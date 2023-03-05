2f16dcb940ac50fd7688d22b46465b848719fa21
Diana-0302 <di.gorkavaya@gmail.com>
2023-03-05 15:23:25 +0200

    add changes

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-const a = Number (prompt('Введите первое число'));
-const b = Number (prompt('Введите второе число'));
+const a = Number(prompt('Введите первое число'));
+const b = Number(prompt('Введите второе число'));
+
+const sum = a + b;
+const diff = a - b;
+const mult = a * b;
+const div = a / b;
 
 
 console.log('Calculations are finished!');
-
-const sum = a + b;
-
 console.log('Sum: ' + a + ' + ' + b + ' = ' + sum);
-
-const diff = a - b;
-
 console.log('Diff: ' + a + ' - ' + b + ' = ' + diff);
-  
-const mult = a * b;
-
 console.log('Mult: ' + a + ' * ' + b + ' = ' + mult);
-
-const div = a / b;
-
 console.log('Div: ' + a + ' / ' + b + ' = ' + div);
