32cdc0ccb0cd400834147480c66bdd70cf553daf
Diana-0302 <di.gorkavaya@gmail.com>
2023-03-13 21:02:14 +0200

    add changes

--- script.js
+++ script.js
@@ -6,10 +6,46 @@ let i;
 let array = [];
 let shouldRepeat;
 
-do {
+function validateOperation() {
   do {
-  operation = prompt('Выберите операцию (введите символ(ы)): \n Сложение: + \n Вычитание: - \n Умножение: * \n Деление: / \n Sin: sin \n Cos: cos \n Возведение в степень: pow \n История вычислений: history');
+    operation = prompt('Выберите операцию (введите символ(ы)): \n Сложение: + \n Вычитание: - \n Умножение: * \n Деление: / \n Sin: sin \n Cos: cos \n Возведение в степень: pow \n История вычислений: history');
   } while(operation !== '+' && operation !== '-' && operation !== '*' && operation !== '/' && operation !== 'pow' && operation !== 'sin'  && operation !== 'cos' && operation !== 'history');
+  return operation;
+}
+
+
+function validateInput(i) {
+  do {
+    number = +prompt(i);
+  } while(number === '' || !number);
+  return number;
+}
+ 
+
+function sum(a, b) {
+  return a + b;
+}
+function diff(a, b) {
+  return a - b;
+}
+function mult(a, b) {
+  return a * b;
+}
+function div(a, b) {
+  return a / b;
+}
+function pow(a, b) {
+  return Math.pow(a, b);
+}
+function sin(a) {
+  return Math.sin(a);
+}
+function cos(a) {
+  return Math.cos(a);
+}
+
+do {
+  operation = validateOperation();
 
   switch(operation) {
     case '+':
@@ -17,50 +53,43 @@ do {
     case '*': 
     case '/': 
     case 'pow':
-      do {
-        a = +prompt('Введите первое число');
-      } while(a === '' || !a);
-
-      do {
-        b = +prompt('Введите второе число');
-      } while(b === '' || !b);
+      a = validateInput('Введите первое число');
+      b = validateInput('Введите второе число');
       break;
 
     case 'sin':
     case 'cos':
-      do {
-        a = +prompt('Введите число в радианах');
-      } while(a === '' || !a);
+      a = validateInput('Введите число в радианах');
       break;
   }
 
   switch(operation) {
     case '+':
-      result = `${a} + ${b} = ${a + b}`;
+      result = `${a} + ${b} = ${sum(a, b)}`;
       array[array.length] = result;
       break;
     case '-': 
-      result = `${a} - ${b} = ${a - b}`; 
+      result = `${a} - ${b} = ${diff(a, b)}`; 
       array[array.length] = result;
       break;
     case '*': 
-      result = `${a} * ${b} = ${a * b}`; 
+      result = `${a} * ${b} = ${mult(a, b)}`; 
       array[array.length] = result;
       break;
     case '/': 
-      result = `${a} / ${b} = ${a / b}`; 
+      result = `${a} / ${b} = ${div(a, b)}`; 
       array[array.length] = result;
       break;
     case 'pow':
-      result = `${a} ^ ${b} = ${Math.pow(a, b)}`; 
+      result = `${a} ^ ${b} = ${pow(a, b)}`; 
       array[array.length] = result;
       break;
     case 'sin':
-      result = `sin ${a} = ${Math.sin(a)}`; 
+      result = `sin ${a} = ${sin(a)}`; 
       array[array.length] = result;
       break;
     case 'cos':
-      result = `cos ${a} = ${Math.cos(a)}`; 
+      result = `cos ${a} = ${cos(a)}`; 
       array[array.length] = result;
       break;
     case 'history':
